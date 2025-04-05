3d89a002c623e97a82e1f8c896ee70946656a2f9
ValdezGabe <valdez.gabe@hotmail.com>
2025-04-05 14:55:46 -0700

    Added supabase client connection, and signup

---
+++ client/src/auth.js
@@ -0,0 +1,32 @@
+export async function signUp(email, password) {
+  // User sign up
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: 'http://localhost:5173/welcome',
+      },
+    });
+    
+    if (error) {
+      return { error };
+    }
+
+    // Insert the user
+    const user =  data.user;
+
+    const { error: insertError } = await supabase.from('users').insert([
+      {
+        uid: user.id,
+        interests: [],
+      },
+    ]);
+
+    if(insertError){
+      return { error: insertError };
+    }
+
+    return { data };
+  }
+
+ 
