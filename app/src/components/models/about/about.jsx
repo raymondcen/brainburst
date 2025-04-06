cca9f42a47d9e7d42e486509b77a3374a5d46176
IvanW5X <ivanwong169@gmail.com>
2025-04-06 05:24:18 -0700

    Finished about page

---
+++ app/src/components/models/about/about.jsx
@@ -0,0 +1,13 @@
+export const About = () => {
+  return (
+    <div className="flex flex-col items-center justify-around w-[50%] m-[20px]">
+      <div className="text-[40px] text-white font-semibold">
+        Welcome to BrainBurst
+      </div>
+      <div className="text-[30px] text-white font-semibold text-balance">
+        Say goodbye to doom scrolling and brain rot – hello to active, addictive
+        learning that keeps you engaged and growing!
+      </div>
+    </div>
+  );
+};
