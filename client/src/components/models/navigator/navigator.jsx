b1e537755bf62f411e6b28c66e7f80089560b19d
ValdezGabe <valdez.gabe@hotmail.com>
2025-04-06 01:35:42 -0700

    Added boilerplate for swiper

--- client/src/components/models/navigator/navigator.jsx
+++ client/src/components/models/navigator/navigator.jsx
@@ -4,11 +4,8 @@ import { IoMdArrowRoundUp, IoMdArrowRoundDown } from "react-icons/io";
 export const Navigator = () => {
   return (
     <div className="flex flex-col w-[70px] h-[150px] bg-[#003a8b] ml-[10px] items-center justify-around rounded-[10px] border-[3px] border-black">
-      <IconButton action={() => console.log("go up")} icon={IoMdArrowRoundUp} />
-      <IconButton
-        action={() => console.log("go down")}
-        icon={IoMdArrowRoundDown}
-      />
+      <IconButton action={slideUp} icon={IoMdArrowRoundUp} />
+      <IconButton action={slideDown} icon={IoMdArrowRoundDown}/>
     </div>
   );
 };
--- client/src/components/ui/swiper.jsx
+++ client/src/components/ui/swiper.jsx
@@ -1,3 +1,33 @@
-export const Swiper = () => {
+import React, { useRef } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import { SwiperCard } from '../models/swiperCard/swiperCard';
+import { Navigator } from '../models/navigator/navigator';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+export const InfinteSwipe = () => {
+
+    const swiperRef = useRef(null);
+
+    return (
+        <div className="flex">
+            <Navigator
+                slideUp={() => swiperRef.current?.slidePrev()}
+                slideDown={() => swiperRef.current?.slideNext()}
+            />
+            <Swiper
+                direction={'vertical'}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+            </Swiper>
+        </div>
+    );
 };
