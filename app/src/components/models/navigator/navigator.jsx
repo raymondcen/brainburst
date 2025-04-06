70d8545ece73061360d5c7ebc830b6e9cf6da4c8
IvanW5X <ivanwong169@gmail.com>
2025-04-06 10:52:21 -0700

    Minor changes

--- app/src/components/models/navigator/navigator.jsx
+++ app/src/components/models/navigator/navigator.jsx
@@ -1,12 +1,11 @@
 import { IconButton } from "../../common/iconButton";
 import { IoMdArrowRoundUp, IoMdArrowRoundDown } from "react-icons/io";
 
-export const Navigator = ({ slideUp, slideDown, currentCard, totalCards }) => {
+export const Navigator = ({ slideUp, slideDown }) => {
   return (
     <div className="flex flex-col items-center justify-around bg-[#003a8b] ml-[10px] rounded-[10px] border-[3px] border-black p-2">
       <IconButton action={slideUp} icon={IoMdArrowRoundUp} />
       <div className="text-white">
-        {totalCards > 0 ? currentCard + 1 : 0} / {totalCards}
       </div>
       <IconButton action={slideDown} icon={IoMdArrowRoundDown} />
     </div>
