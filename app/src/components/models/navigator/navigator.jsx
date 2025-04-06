a69917d106b6a76f241eab69b041bc8647326b01
IvanW5X <ivanwong169@gmail.com>
2025-04-06 05:58:17 -0700

    Navigation component now hidden for smaller widths

--- app/src/components/models/navigator/navigator.jsx
+++ app/src/components/models/navigator/navigator.jsx
@@ -3,7 +3,7 @@ import { IoMdArrowRoundUp, IoMdArrowRoundDown } from "react-icons/io";
 
 export const Navigator = ({ slideUp, slideDown }) => {
   return (
-    <div className="flex flex-col w-[70px] h-[150px] bg-[#003a8b] ml-[10px] items-center justify-around rounded-[10px] border-[3px] border-black">
+    <div className="hidden md:flex flex-col w-[70px] h-[150px] bg-[#003a8b] ml-[10px] items-center justify-around rounded-[10px] border-[3px] border-black">
       <IconButton action={slideUp} icon={IoMdArrowRoundUp} />
       <IconButton action={slideDown} icon={IoMdArrowRoundDown} />
     </div>
